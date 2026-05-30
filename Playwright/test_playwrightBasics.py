295c90ad5394c86350ce879a222481b22ab80ca8
sudhanshudixit03 <sudhanshudixit078@gmail.com>
2026-05-30 15:15:20 +0530

    N

--- Playwright/test_playwrightBasics.py
+++ Playwright/test_playwrightBasics.py
@@ -12,3 +12,26 @@ def test_playwrightBasics(playwright):
 # we can use "page" fixture on the place of above steps , but it always open only "chrome" in "headless"
 def test_playwrightShortcut(page:Page):
     page.goto("https://www.rahulshettyacademy.com")
+
+
+
+                            #use of "get_by_lable" and "get_by_role"
+
+
+# def test_coreLocators(page:Page):
+#     page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+#     page.get_by_label("Username").fill(" Admin")
+#     # page.fill("#Username","rahulshettyacademy")               instead of get_by_label we can use css selectors
+#     page.get_by_label("Password").fill("admin123")
+#     # page.get_by_role("combobox").select_option("teach")          #dropdown - if there were drop down option in which we have to select teacher then on inspect we will use teach option value
+#     # page.click("button[type='submit']")
+#     # # or
+#     # page.get_by_role("button", name="Login").click()
+
+def test_login(page: Page):
+    page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+    page.get_by_placeholder("Username").fill("Admin")
+    page.get_by_placeholder("Password").fill("admin123")
+
+    page.get_by_role("button", name="Login").click()
