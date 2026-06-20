426d7a2a33cfd3d4ca0bb1b25978037f787df49d
sudhanshudixit03 <sudhanshudixit078@gmail.com>
2026-06-21 01:28:42 +0530

    j

--- Playwright/test_moreValidations.py
+++ Playwright/test_moreValidations.py
@@ -3,8 +3,10 @@ import time
 from playwright.sync_api import Page, expect
 
 
-# Test case- Use of Get_by_placeholder
+
 def test_UIChecks(page: Page):
+
+# Scenario- Use of Get_by_placeholder
     page.goto("https://rahulshettyacademy.com/AutomationPractice/")
     page.get_by_role("button", name="Hide").click()
 
@@ -12,11 +14,35 @@ def test_UIChecks(page: Page):
     page.on("dialog", lambda dialog: dialog.accept())
     page.get_by_role("button", name="Confirm").click()
 
+# Scenario- Mouse Hover
+    page.locator("#mousehover").hover()
+    page.get_by_role("link",name="Top").click()
 
-    # test case - Frame handling (example of getting a frame locator)
+
+# Scenario - Frame handling (example of getting a frame locator)
     pageFrame = page.frame_locator("#courses-iframe")
     pageFrame.get_by_role("link",name="All Access plan").click()
     expect(pageFrame.locator("body")).to_contain_text("Happy Subscibers")
     # or we can use get by text
     # expect(pageFrame.get_by_text("Happy Subscibers")).to_be_visible()
 
+
+
+
+
+
+# Scenario- Handle web tables - Check the price of rice is equal to 37
+# Identify the price column
+# identify the rice column
+# extract the price of the rice
+    page.goto("https://rahulshettyacademy.com/seleniumPractise/#/offers")
+
+    for index in range (page.locator('th').count()):
+        if page.locator("th").nth(index).filter(has_text="Price").count()>0 :
+            pricecolValue = index;
+            print(f"Price column value is {pricecolValue}")
+            break
+
+    riceRow = page.locator("tr").filter(has_text="Rice")
+
+    expect(riceRow.locator("td").nth(pricecolValue)).to_have_text("37")
