63c7ca394268a501a79604271881d53554825ab6
sudhanshudixit03 <sudhanshudixit078@gmail.com>
2026-07-04 18:31:46 +0530

    ghf

--- Playwright/test_Network1.py
+++ Playwright/test_Network1.py
@@ -9,7 +9,6 @@ fakePayloadOrderResponse = {"data":[],"message":"No Orders"}
 def intercept_response(route):
     route.fulfill(json = fakePayloadOrderResponse)
 
-@pytest.mark.smoke
 def test_Network_1(page:Page):
     page.goto("https://www.rahulshettyacademy.com/client")
     page.route("https://www.rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/*", intercept_response)
